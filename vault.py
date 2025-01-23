b9f933e7349ed91c3e9bbdfad7e860ebfda52016
bictorbang <victor.wang@ensea.fr>
2025-01-23 01:07:44 +0100

    vault: 242. Valid Anagram

--- vault.py
+++ vault.py
@@ -1,6 +1,12 @@
 from typing import List
+from collections import Counter
 
 # 217. Contains Duplicate
 def containsDuplicate(nums: List[int]):
     return len(set(nums)) < len(nums)
+
+# 242. Valid Anagram
+def isAnagram(s: str, t: str):
+    return Counter(s) == Counter(t)
+
 
