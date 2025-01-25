a7e4857edbc33e26b4af466444b3b446f5cc2c6f
bictorbang <victor.wang@ensea.fr>
2025-01-25 18:28:01 +0100

    feat: integrated leethub v2.0. Next pushes will be automated

--- vault.py
+++ vault.py
@@ -150,3 +150,19 @@ class NumArray:
             return self.a[right]
         return self.a[right] - self.a[left - 1]
 
+# 304. Range Sum Query 2D - Immutable
+
+class NumMatrix:
+    def __init__(self, matrix: List[List[int]]):
+        n = len(matrix[0])
+        m = len(matrix)
+        self.sums = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+        for i in range(m):
+            rowsum = 0
+            for j in range(n): # Assume it exists.
+                rowsum += matrix[i][j]
+                self.sums[i+1][j+1] = self.sums[i][j+1] + rowsum
+   
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        return self.sums[row2+1][col2+1] - self.sums[row1][col2+1] - self.sums[row2+1][col1] + self.sums[row1][col1]
+
