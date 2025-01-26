674ccc2f5e2dd9b19f5c9a9323fc4ce550ab93e4
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-01-26 16:53:08 +0100

    Time: 7 ms (58.59%), Space: 18.8 MB (16.25%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -0,0 +1,11 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        prefix = 0
+        suffix = sum(nums)
+        for i in range(len(nums)):
+            suffix -= nums[i]
+            if prefix == suffix:
+                return i
+            prefix += nums[i]
+
+        return -1
