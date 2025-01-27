10381852f3467885889b162c14306ee16044bf6a
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-01-27 04:29:14 +0100

    Time: 0 ms (100%), Space: 17.9 MB (66.35%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,24 +1,15 @@
 class Solution:
-    def generateAll(self, n):
-        yield from itertools.product(*(["()"] * 2*(n-1))) 
     def generateParenthesis(self, n: int) -> List[str]:
         res = []
-        for p in self.generateAll(n):
-            stack = [1]
-            for elt in p:
-                if elt == ")":
-                    if not stack:
-                        break
-                    stack.pop()
-                else:
-                    stack.append(1)
-                    if len(stack) > n:
-                        break
-            if len(stack) != 1:
-                continue
-            stack.pop()
-            res.append("(" + "".join(p) + ")")
+        def dfs(o, c, s):
+                if o == c and o + c == 2*n:
+                    res.append(s)
+                    return
+                
+                if o < n:
+                    dfs(o + 1, c, s + "(")
+                if c < o:
+                    dfs(o, c + 1, s + ")")
+        dfs(0, 0, "")
+
         return res
-            
-
-
