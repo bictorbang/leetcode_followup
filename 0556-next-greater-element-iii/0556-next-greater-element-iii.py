c4102cbef0191d3b58759574e2743c3a6ba4a165
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-01-28 00:53:32 +0100

    Time: 0 ms (100%), Space: 18 MB (11.55%) - LeetHub

--- 0556-next-greater-element-iii/0556-next-greater-element-iii.py
+++ 0556-next-greater-element-iii/0556-next-greater-element-iii.py
@@ -5,20 +5,16 @@ class Solution:
         m = len(digits)
         swap1, swap2 = 0, 0
         for i in range(m - 1, 0, -1):
-            #print(i)
             if digits[i] > digits[i-1]:
-                #print(f"{digits[i]} > {digits[i-1]}, swap1 = {i-1}")
                 swap1 = i-1
                 for j in range(m-1, i-1, -1):
                     print(j)
                     if digits[j] > digits[i-1]:
-                        #print(f"swap2: {digits[j]} > {digits[i-1]}, swap2 = {j}")
                         swap2 = j
                         break
                 break
         if swap1 == swap2:
             return -1
-        #print(digits, swap1, swap2)
         res = int(digits[:swap1] + digits[swap2] + (digits[swap1 + 1:swap2] + digits[swap1] + digits[swap2+1:])[::-1])
 
         return res if res < n_max else -1
