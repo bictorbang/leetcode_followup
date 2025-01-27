2a055d7e8322d4989f2798a13ec28c0fe8b56439
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-01-27 23:48:22 +0100

    Time: 24 ms (57.51%), Space: 19.3 MB (63.27%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.py
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -1,13 +1,22 @@
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
-        N = len(nums)
-        res = [-1] * N
-        stack = []
-        for i, elt in enumerate(nums):
-            while stack and elt > nums[stack[-1]]:
-                res[stack.pop()] = elt
-            stack.append(i)
-        for i, elt in enumerate(nums):
-            while stack and elt > nums[stack[-1]]:
-                res[stack.pop()] = elt            
+        n = len(nums)
+        res = [-1] * n  # Initialize the result array with -1
+        stack = []      # Monotonic stack to track next greater elements
+        
+        # Iterate through the array twice to simulate circularity
+        for i in range(2 * n - 1, -1, -1):
+            num = nums[i % n]  # Current element (handle circular index)
+            
+            # Remove elements from the stack that are <= the current element
+            while stack and stack[-1] <= num:
+                stack.pop()
+            
+            # If the stack is not empty, the top of the stack is the next greater element
+            if stack:
+                res[i % n] = stack[-1]
+            
+            # Push the current element onto the stack
+            stack.append(num)
+        
         return res
