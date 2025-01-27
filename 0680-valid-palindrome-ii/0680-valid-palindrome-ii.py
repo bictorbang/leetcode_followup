aa1f22b91108d82a0eb4d3e04f6fe4020c342aad
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-01-27 18:42:20 +0100

    Time: 28 ms (93.78%), Space: 18.2 MB (19.97%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -1,13 +1,13 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        String = s
-        Low = 0
-        High = len(String) -1 
+        mid = len(s) // 2
+        s_r = s[::-1]
+        i = 0
+        while i < mid:
+            if s[i] != s_r[i]:
+                a = s[i:mid] == s_r[i+1:mid+1]
+                b = s[i+1:mid+1] == s_r[i:mid]
+                return a or b
+            i += 1
 
-        while Low <= High:
-            if String[Low] == String[High]:
-                Low +=1
-                High -= 1
-            else:
-                return String[Low:High] == String[Low:High][::-1] or String[Low+1:High+1] == String[Low+1:High+1][::-1]
         return True
