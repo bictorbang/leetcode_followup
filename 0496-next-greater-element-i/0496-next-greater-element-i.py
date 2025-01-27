e869ca5da0fe145a7f80292a794815ab9507c104
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-01-27 21:04:38 +0100

    Time: 3 ms (44.77%), Space: 17.9 MB (49.78%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,17 +1,15 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        ans = [-1 for _ in nums1]
+        res = []
         stack = []
-        for i in range(len(nums2)):
-            pivot = nums2[i]
-            while stack and nums1[stack[-1]] < pivot:
-                ans[stack[-1]] = pivot
-                stack.pop()       
-            if pivot in nums1:
-                stack.append(nums1.index(pivot))     
-        return ans
-
-            
+        mapping = {}
+        for n in nums2:
+            while stack and n > stack[-1]:
+                mapping[stack.pop()] = n
+            stack.append(n)
+        for n in nums1:
+            res.append(mapping.get(n, -1))
+        return res     
 
                 
 
