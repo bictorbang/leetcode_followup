0013f655a8e82dd81d9f93e41cfe5cfad72daf3b
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-01-27 18:38:45 +0100

    Time: 27 ms (96.01%), Space: 17.8 MB (64.06%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -1,21 +1,13 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        def remove(s, idx):
-            if idx == 0:
-                return s[1:]
-            if idx == len(s) - 1:
-                return s[:-1]
-            return s[:idx] + s[idx + 1:]
+        String = s
+        Low = 0
+        High = len(String) -1 
 
-        def valid(s, flag):
-            if flag > 1: return False
-            l, r = 0, len(s) - 1
-            while l <= r:
-                if s[l] == s[r]:
-                    l += 1
-                    r -= 1    
-                else:
-                    return (valid(remove(s, l), flag + 1) or valid(remove(s, r), flag + 1))
-            return True
-
-        return valid(s, 0)
+        while Low <= High:
+            if String[Low] == String[High]:
+                Low +=1
+                High -= 1
+            else:
+                return String[Low:High] == String[Low:High][::-1] or String[Low+1:High+1] == String[Low+1:High+1][::-1]
+        return True
