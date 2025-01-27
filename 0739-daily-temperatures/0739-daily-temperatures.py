cfae69603ff9419dab056773ba46919a83c8ea25
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-01-27 17:16:20 +0100

    Time: 104 ms (63.46%), Space: 27.5 MB (53.38%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -0,0 +1,12 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n = len(temperatures)
+        answer = [0 for _ in range(n)]
+        stack = [0]
+        for i in range(1, n):
+            pivot = temperatures[i]    
+            while (stack and temperatures[stack[-1]] < pivot):
+                answer[stack[-1]] = i - stack[-1]
+                stack.pop()
+            stack.append(i)
+        return answer
