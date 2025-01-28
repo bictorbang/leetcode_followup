16accbe7cf04c084048a8fe764c6551e02baa8df
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-01-28 01:03:46 +0100

    Time: 0 ms (100%), Space: 17.9 MB (23.53%) - LeetHub

--- 0556-next-greater-element-iii/0556-next-greater-element-iii.py
+++ 0556-next-greater-element-iii/0556-next-greater-element-iii.py
@@ -1,21 +1,12 @@
 class Solution:
     def nextGreaterElement(self, n: int) -> int:
         n_max = 2**31
-        digits = str(n)
-        m = len(digits)
-        swap1, swap2 = 0, 0
-        for i in range(m - 1, 0, -1):
-            if digits[i] > digits[i-1]:
-                swap1 = i-1
-                for j in range(m-1, i-1, -1):
-                    if digits[j] > digits[i-1]:
-                        swap2 = j
-                        break
-                break
-        if swap1 == swap2:
-            return -1
-        res = int(digits[:swap1] + digits[swap2] + (digits[swap1 + 1:swap2] + digits[swap1] + digits[swap2+1:])[::-1])
-
-        return res if res < n_max else -1
-
+        s = str(n)
+        for i in range(len(s) - 2, -1, -1):
+            if s[i] < s[i + 1]:
+                for k in range(len(s) - 1, i, -1):
+                    if s[i] < s[k]:
+                        res = int(s[:i] + s[k] + s[-1:k:-1] + s[i] + s[k-1:i:-1])
+                        return res if res < n_max else -1
+        return -1
 
