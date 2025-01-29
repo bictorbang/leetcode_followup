3a652b07aabf4b6044c1059813d8ec7a609ec35f
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-01-29 01:42:14 +0100

    Time: 0 ms (100%), Space: 17.7 MB (63.1%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
@@ -4,15 +4,6 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-
-    def rev(self, cur, prev):
-            if not cur:
-                return prev
-            next_ = cur.next
-            cur.next = prev
-            prev = cur
-            return self.rev(next_, prev)
-
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
         if not head or left == right:
             return head 
