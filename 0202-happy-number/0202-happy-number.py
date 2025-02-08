ce5522635b2b2121f57c279d4e2ba2c4b47417d6
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-02-08 17:58:01 +0100

    Time: 4 ms (16.44%), Space: 17.9 MB (15.64%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        n_happy = n
+        ht = set()
+        while n_happy != 1:
+            n_happy = sum([int(i)*int(i) for i in str(n_happy)])
+            if n_happy in ht:
+                return False
+            ht.add(n_happy)
+        return True
+
+        
