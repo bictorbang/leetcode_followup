5ca3fd63d13796353e0f126902223e2ba3efc6b7
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-02-08 17:59:37 +0100

    Time: 1 ms (56.39%), Space: 17.7 MB (54.13%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -3,7 +3,7 @@ class Solution:
         n_happy = n
         ht = set()
         while n_happy != 1:
-            n_happy = sum([int(i)*int(i) for i in str(n_happy)])
+            n_happy = sum((int(i)*int(i) for i in str(n_happy)))
             if n_happy in ht:
                 return False
             ht.add(n_happy)
