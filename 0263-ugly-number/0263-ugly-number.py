fb4376052611dcf7e411b76cb92f5b43b2afb9b9
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-02-08 18:37:50 +0100

    Time: 0 ms (100%), Space: 17.6 MB (83.26%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.py
+++ 0263-ugly-number/0263-ugly-number.py
@@ -1,21 +1,10 @@
 class Solution:
     def isUgly(self, n: int) -> bool:
-        if n == 1: return True
-        if n < 1: return False
-        def prime_factors(n):
-            if n % 2 == 0:
-                yield 2
-                while n % 2 == 0:
-                    n = n // 2
-            i = 3
-            while i * i <= n:
-                if n % i == 0:
-                    yield i
-                    while n % i == 0:
-                        n = n // i
-                i += 2
-            if n > 2:
-                yield n
-        for prime in prime_factors(n):
-            if prime > 5: return False
-        return True
+        if n <= 0:
+            return False
+
+        for factor in [2, 3, 5]:
+            while n % factor == 0:
+                n //= factor
+        
+        return n == 1
