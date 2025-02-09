8d6772dd9fe31a6e72c49d230153faefc3f621ea
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-02-09 16:39:09 +0100

    Time: 3 ms (73.14%), Space: 17.9 MB (51.69%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -6,17 +6,18 @@
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         res = ListNode()
-        def addDigits(l1, l2, res, carry = 0):
-            if not l1 and not l2 and not carry:
-                return res
-            l1_val = 0 if not l1 else l1.val
-            l2_val = 0 if not l2 else l2.val
-            res.next = ListNode(s := (l1_val + l2_val + carry))
-            carry = 1 if s > 9 else 0
-            res.next.val %= 10
-            l1_next = l1.next if l1 else None
-            l2_next = l2.next if l2 else None
-            addDigits(l1_next, l2_next, res.next, carry)
-        addDigits(l1, l2, res)
+        cur = res
+        carry = 0
+        while l1 or l2 or carry:
+            val = carry
+            if l1: 
+                val += l1.val
+                l1 = l1.next
+            if l2: 
+                val += l2.val
+                l2 = l2.next
+            carry, val = divmod(val, 10)
+            cur.next = ListNode(val)
+            cur = cur.next
         return res.next
         
