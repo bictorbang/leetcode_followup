560ef89860915b3991952266cc8b00962803d227
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-02-10 17:15:53 +0100

    Time: 594 ms (42.37%), Space: 67.4 MB (23.93%) - LeetHub

--- 2024-calculate-special-bonus/2024-calculate-special-bonus.py
+++ 2024-calculate-special-bonus/2024-calculate-special-bonus.py
@@ -2,5 +2,5 @@ import pandas as pd
 
 def calculate_special_bonus(employees: pd.DataFrame) -> pd.DataFrame:
     employees["bonus"] = 0
-    employees.loc[(employees.name.apply(lambda x: x[0]) != "M")&(employees.employee_id % 2 == 1), "bonus"] = employees["salary"]
+    employees.loc[(employees.name.str[0] != "M")&(employees.employee_id % 2 == 1), "bonus"] = employees["salary"]
     return employees[["employee_id", "bonus"]].sort_values("employee_id")
