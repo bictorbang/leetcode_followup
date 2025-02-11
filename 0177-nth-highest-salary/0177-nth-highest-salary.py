8cc89d6360968746e4a79f203cf3936c5e20f69f
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-02-11 15:57:52 +0100

    Time: 279 ms (93.48%), Space: 67.1 MB (14.03%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.py
+++ 0177-nth-highest-salary/0177-nth-highest-salary.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
 def nth_highest_salary(employee: pd.DataFrame, N: int) -> pd.DataFrame:
-    if N < 1 or N > len(e := employee[["salary"]].drop_duplicates()): return pd.DataFrame({f"getNthHighestSalary({N})": [None]})
+    e = employee[["salary"]].drop_duplicates()
+    if N < 1 or N > len(e): return pd.DataFrame({f"getNthHighestSalary({N})": [None]})
     return e.sort_values(by = "salary", ascending = False).iloc[[N-1]].rename({"salary" : f"getNthHighestSalary({N})"}, axis = 1)
