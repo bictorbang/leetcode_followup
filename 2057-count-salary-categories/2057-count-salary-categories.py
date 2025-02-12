82b156bf2ae751f75e0bb63acf56957819155cb2
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-02-12 19:53:49 +0100

    Time: 988 ms (8.01%), Space: 90.3 MB (34.88%) - LeetHub

--- 2057-count-salary-categories/2057-count-salary-categories.py
+++ 2057-count-salary-categories/2057-count-salary-categories.py
@@ -3,3 +3,4 @@ import pandas as pd
 def count_salary_categories(accounts: pd.DataFrame) -> pd.DataFrame:
     accounts["income"]= pd.cut(accounts["income"], bins = [0, 20000, 50001, float("inf")], labels = ["Low Salary", "Average Salary", "High Salary"], right = False)
     return pd.DataFrame(accounts["income"].value_counts().reset_index()).rename({"income": "category", "count" : "accounts_count"}, axis = 1)
+#
