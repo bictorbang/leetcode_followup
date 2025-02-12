f9eef944c2a8215932646e9e3fe45962eb8e2148
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-02-13 00:19:02 +0100

    Time: 414 ms (91.3%), Space: 68.2 MB (34.66%) - LeetHub

--- 0607-sales-person/0607-sales-person.py
+++ 0607-sales-person/0607-sales-person.py
@@ -1,6 +1,4 @@
 import pandas as pd
 
 def sales_person(sales_person: pd.DataFrame, company: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
-    df = orders.drop("order_date", axis = 1).merge(sales_person[["sales_id", "name"]], how = "right").merge(company[["com_id", "name"]], on = "com_id", suffixes = (None, "_y"), how = "left")
-    names = df[df["name_y"] == "RED"]["name"].values
-    return df.drop(df[df["name"].isin(names)].index)[["name"]].drop_duplicates()
+    return sales_person[~sales_person["sales_id"].isin(orders.merge(company[company["name"] == "RED"], on = "com_id")["sales_id"].unique())][["name"]]
