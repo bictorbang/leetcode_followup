d0a4e2235511eb2e053ab6d70dc671fe3d591009
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-02-12 19:58:33 +0100

    Time: 295 ms (90.42%), Space: 67 MB (90.06%) - LeetHub

--- 0178-rank-scores/0178-rank-scores.py
+++ 0178-rank-scores/0178-rank-scores.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
 def order_scores(scores: pd.DataFrame) -> pd.DataFrame:
-    s = scores[["score"]].sort_values(by = "score", ascending = False)
-    s["rank"] = (s != s.shift().fillna(-1)).cumsum()
-    return s
+    scores["rank"] = scores["score"].rank(method = "dense", ascending = False)
+    return scores[["score", "rank"]].sort_values(by = "rank")
