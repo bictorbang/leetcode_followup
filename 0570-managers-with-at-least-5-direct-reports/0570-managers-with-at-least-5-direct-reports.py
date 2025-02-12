a20cb071fde3a6fd497f40861c08bc6d1efc6f20
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-02-13 00:36:19 +0100

    Time: 487 ms (58.63%), Space: 67.9 MB (84.65%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.py
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 def find_managers(employee: pd.DataFrame) -> pd.DataFrame:
-    ag = employee.groupby("managerId").agg(cnt = ("managerId", "count")).reset_index()
+    ag = employee.groupby("managerId").size().reset_index(name = "cnt")
     ag = ag[ag["cnt"] > 4]["managerId"].values
     return employee[employee["id"].isin(ag)][["name"]]
