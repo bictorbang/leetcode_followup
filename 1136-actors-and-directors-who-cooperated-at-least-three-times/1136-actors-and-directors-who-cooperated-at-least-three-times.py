c58574e1114f2dd0f179341cc083e83393bb685c
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-02-12 19:07:11 +0100

    Time: 446 ms (69.96%), Space: 67.1 MB (60.63%) - LeetHub

--- 1136-actors-and-directors-who-cooperated-at-least-three-times/1136-actors-and-directors-who-cooperated-at-least-three-times.py
+++ 1136-actors-and-directors-who-cooperated-at-least-three-times/1136-actors-and-directors-who-cooperated-at-least-three-times.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 def actors_and_directors(actor_director: pd.DataFrame) -> pd.DataFrame:
     actor_director = actor_director.groupby(["actor_id", "director_id"]).count().reset_index()
-    return actor_director[actor_director.timestamp > 2].drop("timestamp", axis = 1)
+    return actor_director[actor_director.timestamp > 2][["actor_id", "director_id"]]
