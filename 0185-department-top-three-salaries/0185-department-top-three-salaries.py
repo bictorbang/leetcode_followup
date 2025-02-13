b8e6157b5a187a7930ed7f5d4fe8f7390f394a0a
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-02-13 01:24:27 +0100

    Time: 622 ms (58.17%), Space: 69 MB (26.47%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.py
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.py
@@ -1,13 +1,6 @@
 import pandas as pd
 
 def top_three_salaries(employee: pd.DataFrame, department: pd.DataFrame) -> pd.DataFrame:
-    #employee = employee.rename({"name": "Employee"}, axis = 1)
-    #return employee.merge(department, left_on = "departmentId", right_on = "id").rename({"name" : "Department", "salary": "Salary"}, axis = 1)[["Department", "Employee", "Salary"]].groupby("Department").apply(lambda x: x[x["Salary"] == x["Salary"].max()])
     employee = employee.rename({"name" : "Employee"}, axis = 1)
     df = employee.merge(department, left_on = "departmentId", right_on = "id").rename({"name" : "Department", "salary": "Salary"}, axis = 1)[["Department", "Employee", "Salary"]]
-    print(df)
-    #df = df.groupby("Department").apply(lambda x : x[x["Salary"].isin(x[["Salary"]].drop_duplicates().sort_values(by = "Salary", ascending = False).head(3))])
-    df_ = df[["Department", "Salary"]].drop_duplicates().sort_values("Salary", ascending = False).groupby("Department").head(3)
-    print(df_)
-
-    return df.merge(df_, on = ["Department", "Salary"])
+    return df.merge(df[["Department", "Salary"]].drop_duplicates().sort_values("Salary", ascending = False).groupby("Department").head(3), on = ["Department", "Salary"])
