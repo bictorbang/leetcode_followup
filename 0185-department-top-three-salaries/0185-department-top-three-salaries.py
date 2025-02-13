1068beac28ca1549028ee8081e34a76515b57460
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-02-13 01:30:48 +0100

    Time: 622 ms (58.17%), Space: 69 MB (26.47%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.py
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.py
@@ -3,4 +3,4 @@ import pandas as pd
 def top_three_salaries(employee: pd.DataFrame, department: pd.DataFrame) -> pd.DataFrame:
     employee = employee.rename({"name" : "Employee"}, axis = 1)
     df = employee.merge(department, left_on = "departmentId", right_on = "id").rename({"name" : "Department", "salary": "Salary"}, axis = 1)[["Department", "Employee", "Salary"]]
-    return df.merge(df.drop("Employee", axis = 1).drop_duplicates().sort_values("Salary", ascending = False).groupby("Department").head(3), on = ["Department", "Salary"])
+    return df.merge(df[["Department", "Salary"]].drop_duplicates().sort_values("Salary", ascending = False).groupby("Department").head(3), on = ["Department", "Salary"])
