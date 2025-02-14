b8374816aaa77c4874c220c2ae22e3b3d6d5ab50
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-02-15 00:32:26 +0100

    Time: 881 ms (5.03%), Space: 66.7 MB (39.78%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.py
+++ 0584-find-customer-referee/0584-find-customer-referee.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
 def find_customer_referee(customer: pd.DataFrame) -> pd.DataFrame:
-    return customer.loc[(customer["referee_id"].fillna(0) != 2), ["name"]]
+    return customer[(customer["referee_id"].fillna(0) != 2)][["name"]]
