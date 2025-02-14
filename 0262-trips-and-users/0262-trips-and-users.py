947fa455fa32d095600935c87330509c5b055141
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-02-14 20:52:07 +0100

    Time: 486 ms (79.25%), Space: 67 MB (90.34%) - LeetHub

--- 0262-trips-and-users/0262-trips-and-users.py
+++ 0262-trips-and-users/0262-trips-and-users.py
@@ -1,11 +1,7 @@
 import pandas as pd
 
-def trips_and_users(trips: pd.DataFrame, users: pd.DataFrame) -> pd.DataFrame:
+def trips_and_users(trips: pd.DataFrame, users: pd.DataFrame) -> pd.DataFrame: 
     trips = trips[(trips["request_at"] >= "2013-10-01") & (trips["request_at"] <= "2013-10-03")]
     mask = users[users["banned"] == "Yes"]["users_id"]
     trips = trips[(~trips["client_id"].isin(mask)) & (~trips["driver_id"].isin(mask))]
-    cancelled = trips[trips["status"] != "completed"][["request_at", "status"]].groupby("request_at").count().reset_index()
-    total = trips[["request_at", "status"]].groupby("request_at").count().reset_index()
-    end = total.merge(cancelled, on = "request_at", how = "left").rename(columns = {"request_at" : "Day"}).fillna(0)
-    end["Cancellation Rate"] = round(end["status_y"] / end["status_x"], 2)
-    return end[["Day", "Cancellation Rate"]]
+    return trips.groupby("request_at")["status"].apply(lambda x : x.str.contains("cancelled").mean().round(2)).reset_index().rename(columns={"request_at":"Day","status":"Cancellation Rate"})
