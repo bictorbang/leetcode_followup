c89893f5ec27f6cbd69e5e1a47e31413d145a61c
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-02-14 17:51:49 +0100

    Time: 270 ms (98.9%), Space: 61.1 MB (100%) - LeetHub

--- 1827-invalid-tweets/1827-invalid-tweets.py
+++ 1827-invalid-tweets/1827-invalid-tweets.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 def invalid_tweets(tweets: pd.DataFrame) -> pd.DataFrame:
-    return tweets[tweets["content"].str.len() > 15][["tweet_id"]]
+    return tweets.loc[tweets.content.apply(lambda x : np.nan if len(x)>15 else 0).isna(), ["tweet_id"]]
