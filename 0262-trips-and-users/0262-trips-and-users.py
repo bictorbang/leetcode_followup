fddedb8d332e5ace3103f5c299b26d461465395a
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-02-14 20:46:25 +0100

    Time: 586 ms (56.35%), Space: 67.7 MB (61.36%) - LeetHub

--- 0262-trips-and-users/0262-trips-and-users.py
+++ 0262-trips-and-users/0262-trips-and-users.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 def trips_and_users(trips: pd.DataFrame, users: pd.DataFrame) -> pd.DataFrame:
     trips = trips[(trips["request_at"] >= "2013-10-01") & (trips["request_at"] <= "2013-10-03")]
-    trips = trips[~trips["client_id"].isin(users[(users["banned"] == "Yes")&(users["role"] == "client")]["users_id"])]
-    trips = trips[~trips["driver_id"].isin(users[(users["banned"] == "Yes")&(users["role"] == "driver")]["users_id"])]
+    mask = users[users["banned"] == "Yes"]["users_id"]
+    trips = trips[(~trips["client_id"].isin(mask)) & (~trips["driver_id"].isin(mask))]
     cancelled = trips[trips["status"] != "completed"][["request_at", "status"]].groupby("request_at").count().reset_index()
     total = trips[["request_at", "status"]].groupby("request_at").count().reset_index()
     end = total.merge(cancelled, on = "request_at", how = "left").rename(columns = {"request_at" : "Day"}).fillna(0)
