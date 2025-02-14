878bae073e7fcf2c7e43b8f846169945cca4b0b0
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-02-14 18:27:01 +0100

    Time: 320 ms (93.13%), Space: 66.8 MB (39.78%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.py
+++ 0180-consecutive-numbers/0180-consecutive-numbers.py
@@ -2,4 +2,3 @@ import pandas as pd
 
 def consecutive_numbers(logs: pd.DataFrame) -> pd.DataFrame:
     return logs[["num"]][(logs["num"] == logs["num"].shift()) & (logs["num"] == logs["num"].shift(2))].rename({"num" : "ConsecutiveNums"}, axis = 1).drop_duplicates()
-    #return logs[["num"]].rename({"num": "ConsecutiveNums"}, axis = 1)[((logs == logs.shift()) & (logs == logs.shift(2)))].dropna()
