cf25d890ac50c1089d7c2842f3f5a559a6dd7495
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-02-14 19:57:11 +0100

    Time: 436 ms (77.49%), Space: 67.5 MB (34.64%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.py
+++ 0197-rising-temperature/0197-rising-temperature.py
@@ -2,4 +2,5 @@ import pandas as pd
 
 def rising_temperature(weather: pd.DataFrame) -> pd.DataFrame:
     weather = weather.sort_values(by = "recordDate")
-    return weather[(weather["temperature"].diff() > 0) & (weather["recordDate"].diff().dt.days == 1)][["id"]]
+    weather["diff"] = weather["recordDate"] - weather.shift()["recordDate"]
+    return weather[(weather["temperature"] > weather["temperature"].shift()) & (weather["diff"]==pd.Timedelta(days = 1))][["id"]]
