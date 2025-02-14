7da3f9d4d561bb62e242d15646a21ab865b7e36f
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-02-14 20:41:22 +0100

    Time: 774 ms (19.32%), Space: 65.3 MB (100%) - LeetHub

--- 0262-trips-and-users/0262-trips-and-users.py
+++ 0262-trips-and-users/0262-trips-and-users.py
@@ -1,10 +1,6 @@
 import pandas as pd
 
 def trips_and_users(trips: pd.DataFrame, users: pd.DataFrame) -> pd.DataFrame:
-    #users["client_id"] = users[users["role"] == "client"]["users_id"]
-    #users["driver_id"] = users[users["role"] == "driver"]["users_id"]
-    #~trips["client_id"].isin(users[(users["banned"])&(users["role"] == "client")])
-    #~trips["driver_id"].isin(users[(users["banned"])&(users["role"] == "driver")])
     trips = trips[(trips["request_at"] >= "2013-10-01") & (trips["request_at"] <= "2013-10-03")]
     trips = trips[~trips["client_id"].isin(users[(users["banned"] == "Yes")&(users["role"] == "client")]["users_id"])]
     trips = trips[~trips["driver_id"].isin(users[(users["banned"] == "Yes")&(users["role"] == "driver")]["users_id"])]
