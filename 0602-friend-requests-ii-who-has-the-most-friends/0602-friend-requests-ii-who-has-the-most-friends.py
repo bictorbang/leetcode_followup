9936aa18425a6a587af3350fb6b24f141ddd4187
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-02-15 03:38:49 +0100

    Time: 283 ms (95.86%), Space: 66.7 MB (53.5%) - LeetHub

--- 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.py
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.py
@@ -3,5 +3,4 @@ import pandas as pd
 def most_friends(request_accepted: pd.DataFrame) -> pd.DataFrame:
     persons = pd.concat([request_accepted["requester_id"], request_accepted["accepter_id"]])
     persons = persons.groupby(persons).size().reset_index().rename({"index" : "id", 0 : "num"}, axis = 1)
-    print(persons)
     return persons[persons["num"] == persons["num"].max()]
