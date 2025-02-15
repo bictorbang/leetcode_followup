51f7fbe46636f1c197d215a169347ad454691e82
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-02-15 03:57:16 +0100

    Time: 284 ms (94.86%), Space: 61.6 MB (99.98%) - LeetHub

--- 1258-article-views-i/1258-article-views-i.py
+++ 1258-article-views-i/1258-article-views-i.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
 def article_views(views: pd.DataFrame) -> pd.DataFrame:
-    return views[views["author_id"] == views["viewer_id"]][["author_id"]].drop_duplicates().sort_values(by = "author_id").rename(columns = {"author_id" : "id"})
+    return views.loc[(views.author_id == views.viewer_id), ["author_id"]].rename(columns = {"author_id" : "id"}).drop_duplicates().sort_values(by = "id", ascending = True)
