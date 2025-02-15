536696617296926289ecf09a9afb72db2c8e4612
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-02-16 00:26:02 +0100

    Time: 1173 ms (36.57%), Space: 312.4 MB (5.14%) - LeetHub

--- 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.py
+++ 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.py
@@ -9,7 +9,7 @@ class Solution:
                 total.add(target*target)
                 return
             for i in range(1, len(k)):
-                partition_dfs(k[i:], pre_sum + int(k[ :i]), target)
+                partition_dfs(k[i:], pre_sum + int(k[:i]), target)
         total = {1} # 1 is always gonna work for 1 <= n <= 1000
         for i in range(2, n + 1):
             s = str(i*i)
