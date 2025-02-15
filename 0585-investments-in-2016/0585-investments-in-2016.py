0bf045189d1434c1f4ccd2b0d989e80dd8c870ef
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-02-15 01:14:19 +0100

    Time: 431 ms (88.33%), Space: 67 MB (64.22%) - LeetHub

--- 0585-investments-in-2016/0585-investments-in-2016.py
+++ 0585-investments-in-2016/0585-investments-in-2016.py
@@ -4,3 +4,5 @@ def find_investments(insurance: pd.DataFrame) -> pd.DataFrame:
     city = insurance.duplicated(["lat", "lon"], keep = False)
     tiv_2015 = insurance.duplicated("tiv_2015", keep = False)
     return insurance[(~city)&(tiv_2015)][["tiv_2016"]].sum().to_frame(name = "tiv_2016")
+    #return pd.DataFrame({"tiv_2016" : [round(insurance.drop_duplicates(["lat", "lon"])[insurance.duplicated("tiv_2015", keep = False)][["tiv_2016"]].sum().astype(float), 2)]})
+
