b71e690236c789ad147e5a463e537d48b702adc1
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-02-16 01:05:00 +0100

    Time: 95 ms (76.57%), Space: 17.9 MB (55.14%) - LeetHub

--- 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.py
+++ 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.py
@@ -1,18 +1,12 @@
-from functools import cache
 class Solution:
     def punishmentNumber(self, n: int) -> int:
-        @cache
-        def partition_dfs(k, pre_sum, target): 
-            if not k or found: return
-            if int(k) + pre_sum == target: 
-                found.append(True)
-                total.add(target*target)
-                return
-            for i in range(1, len(k)):
-                partition_dfs(k[i:], pre_sum + int(k[:i]), target)
-        total = {1} # 1 is always gonna work for 1 <= n <= 1000
+        def partition(k, target): 
+            if target < 0 or k < target: return
+            if k == target: return True
+            return (partition(k // 10, target - k % 10) or partition(k // 100, target - k % 100) or partition(k // 1000, target - k % 1000))
+        total = 1 # 1 is always gonna work for 1 <= n <= 1000
         for i in range(2, n + 1):
-            s = str(i*i)
-            found = []
-            partition_dfs(s, 0, i)
-        return sum(total)
+            s = i*i
+            if partition(s, i):
+                total += s
+        return total
