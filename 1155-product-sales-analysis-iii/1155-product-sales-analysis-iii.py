f3dad521411fc80b751af0e86290daaf28ef50be
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-02-18 03:01:48 +0100

    Time: 437 ms (85.94%), Space: 69.7 MB (43.06%) - LeetHub

---
+++ 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+def sales_analysis(sales: pd.DataFrame, product: pd.DataFrame) -> pd.DataFrame:
+    sales["rank"] = sales.groupby("product_id")["year"].rank(method = "dense")
+    return sales.loc[sales["rank"] == 1].merge(product).iloc[:, [1, 2, 3, 4]].rename(columns = {"year": "first_year"})
