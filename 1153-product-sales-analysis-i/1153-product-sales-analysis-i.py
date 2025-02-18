8c14b20008e53ee1adfda0aed59517d911e6abbd
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-02-18 01:52:03 +0100

    Time: 600 ms (54.84%), Space: 69.9 MB (40.28%) - LeetHub

--- 1153-product-sales-analysis-i/1153-product-sales-analysis-i.py
+++ 1153-product-sales-analysis-i/1153-product-sales-analysis-i.py
@@ -1,4 +1,5 @@
 import pandas as pd
 
 def sales_analysis(sales: pd.DataFrame, product: pd.DataFrame) -> pd.DataFrame:
-    return sales.merge(product)[['product_name','year','price']]
+    merged = sales.merge(product, on = "product_id", how = "left")
+    return merged[["product_name", "year", "price"]]
