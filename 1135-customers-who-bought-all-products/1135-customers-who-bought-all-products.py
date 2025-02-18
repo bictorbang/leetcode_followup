f3e9551686c20d96a019acc7991ba92eb986cafd
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-02-18 01:42:53 +0100

    Time: 337 ms (86.28%), Space: 67.6 MB (30.18%) - LeetHub

--- 1135-customers-who-bought-all-products/1135-customers-who-bought-all-products.py
+++ 1135-customers-who-bought-all-products/1135-customers-who-bought-all-products.py
@@ -2,5 +2,5 @@ import pandas as pd
 
 def find_customers(customer: pd.DataFrame, product: pd.DataFrame) -> pd.DataFrame:
     products = len(product)
-    cnt = customer.drop_duplicates().groupby("customer_id").count().reset_index()
+    cnt = customer.groupby("customer_id").nunique().reset_index()
     return cnt[cnt["product_key"] == products][["customer_id"]]
