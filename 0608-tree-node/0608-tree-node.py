73579ff69bab316c6613ebd5f2a84a8f61f17330
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-02-18 01:16:37 +0100

    Time: 301 ms (98.36%), Space: 66.6 MB (57.51%) - LeetHub

--- 0608-tree-node/0608-tree-node.py
+++ 0608-tree-node/0608-tree-node.py
@@ -2,10 +2,7 @@ import pandas as pd
 import numpy as np
 
 def tree_node(tree: pd.DataFrame) -> pd.DataFrame:
-    parents = tree.p_id.unique()
-    def tree_type(x):
-        if pd.isna(x.p_id): return "Root"
-        if x.id not in parents: return "Leaf"
-        return "Inner"
-    tree["type"] = tree.apply(tree_type, axis = 1)
-    return tree[["id", "type"]]
+    tree.loc[tree['id'].isin(tree['p_id']), "type"] = "Inner" 
+    tree.loc[tree['p_id'].isna(), "type"] = "Root" ###
+    tree.loc[tree['type'].isna(), "type"] = "Leaf" 
+    return (tree[['id', 'type']])
