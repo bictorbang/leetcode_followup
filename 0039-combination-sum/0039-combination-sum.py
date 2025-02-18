c99d1f6915cd280896e7d0d4c52f3c267ee56d71
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-02-18 18:39:32 +0100

    Time: 11 ms (60.82%), Space: 17.8 MB (61.21%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,6 +1,6 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        res = []
+        res, s = [], []
         n = len(candidates)
         def dfs(idx, s, total):
             if total == target:
@@ -12,4 +12,4 @@ class Solution:
             s.pop()
             dfs(idx + 1, s, total)
             return res
-        return dfs(0, [], 0)
+        return dfs(0, s, 0)
