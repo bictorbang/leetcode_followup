f1ecdb1a754558734811e0be48d13acc8a4d2306
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-02-18 01:33:52 +0100

    Time: 463 ms (58.03%), Space: 66.4 MB (46.25%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.py
+++ 0626-exchange-seats/0626-exchange-seats.py
@@ -4,4 +4,4 @@ def exchange_seats(seat: pd.DataFrame) -> pd.DataFrame:
     seat.loc[(seat.id % 2 == 1) ,  "new"] = seat.student.shift(-1)
     seat.loc[(seat.id % 2 == 0) ,  "new"] = seat.student.shift()
     seat["student"] = seat.apply((lambda x: x.new if not pd.isna(x.new) else x.student), axis = 1)
-    return seat[["id", "student"]]
+    return seat.drop("new", axis = 1)
