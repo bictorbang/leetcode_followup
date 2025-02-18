b06c6df7809125c596a349250952b80d61921ff1
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-02-18 01:21:21 +0100

    Time: 241 ms (99.71%), Space: 66.5 MB (43.79%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.py
+++ 0619-biggest-single-number/0619-biggest-single-number.py
@@ -4,4 +4,4 @@ def biggest_single_number(my_numbers: pd.DataFrame) -> pd.DataFrame:
     dup = my_numbers.drop_duplicates(keep = False)
     if len(dup) == 0: 
         return pd.DataFrame({"num" : [None]})
-    return dup.sort_values("num", ascending = False).head(1)
+    return dup.max().to_frame(name = "num")
