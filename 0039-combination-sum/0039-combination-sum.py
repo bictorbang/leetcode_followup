f642408ee2d898580988bd529dbf042f52d33100
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-02-18 18:38:37 +0100

    Time: 13 ms (37.51%), Space: 18.1 MB (19.73%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,19 +1,15 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
-        hashmap = []
-        def dfs(s, target):
-            if s is not None:
-                if sum(s) > target: return
-                if sum(s) == target:
-                    if sorted(s) not in hashmap:
-                        hashmap.append(sorted(s))
-                        res.append(s)
-                    return
-            for elt in candidates:
-                s2 = s[:] if s is not None else []
-                s2.append(elt)
-                dfs(s2, target)
-        dfs([], target)
-        return res
-
+        n = len(candidates)
+        def dfs(idx, s, total):
+            if total == target:
+                res.append(s[:])
+                return
+            if total > target or idx >= n: return
+            s.append(candidates[idx])
+            dfs(idx, s, total + candidates[idx])
+            s.pop()
+            dfs(idx + 1, s, total)
+            return res
+        return dfs(0, [], 0)
