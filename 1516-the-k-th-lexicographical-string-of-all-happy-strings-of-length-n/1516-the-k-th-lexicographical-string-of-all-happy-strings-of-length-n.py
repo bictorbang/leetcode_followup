46489b3553dc2248acbc653cd07b14571147a846
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-02-19 01:38:55 +0100

    Time: 9 ms (57.33%), Space: 18.1 MB (42.89%) - LeetHub

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
@@ -4,6 +4,7 @@ class Solution:
         res = []
         def dfs(cur):
             if len(cur) > 1 and cur[-1] == cur[-2]: return
+            if self.idx > k: return
             if len(cur) == n: 
                 self.idx += 1
                 if self.idx == k: 
