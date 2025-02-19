90fd73977dba8de05e2094023b02b522500c592d
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-02-19 17:25:06 +0100

    Time: 191 ms (8.22%), Space: 17.9 MB (60.27%) - LeetHub

--- 2883-partition-string-into-minimum-beautiful-substrings/2883-partition-string-into-minimum-beautiful-substrings.py
+++ 2883-partition-string-into-minimum-beautiful-substrings/2883-partition-string-into-minimum-beautiful-substrings.py
@@ -1,13 +1,15 @@
 class Solution:
     def minimumBeautifulSubstrings(self, s: str) -> int:
         if s[0] == "0": return -1
-
+        self.found = False
         powers_of_5 = [str(bin(5**x))[2:] for x in range(6, -1, -1)]
         cur = ""
         def dfs(cur, idx):
+            if self.found: return
             if len(cur) > len(s): return
             if cur == s:
-                return idx
+                self.found = idx
+                return self.found
             for elt in powers_of_5:
                 step = dfs(cur + elt, idx + 1)
                 if step:
