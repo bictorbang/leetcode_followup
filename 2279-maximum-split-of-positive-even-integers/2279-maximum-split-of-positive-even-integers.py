32b8593435626726e9d1185c27a316818a4963b9
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-02-21 03:10:16 +0100

    Time: 15 ms (98.5%), Space: 28.8 MB (19.85%) - LeetHub

--- 2279-maximum-split-of-positive-even-integers/2279-maximum-split-of-positive-even-integers.py
+++ 2279-maximum-split-of-positive-even-integers/2279-maximum-split-of-positive-even-integers.py
@@ -2,10 +2,7 @@ class Solution:
     def maximumEvenSplit(self, finalSum: int) -> List[int]:
         if (finalSum%2): return []
         if finalSum < 5: return [finalSum]
-        finalSum //= 2
-
-        N = int((-1 + (1 + 8 * finalSum)**(0.5)) / 2)
+        N = int((-1 + (1 + 4 * finalSum)**(0.5)) / 2)
         result = list(range(2, 2*N+1, 2))
-        result[-1] += 2 * (finalSum - N * (N + 1) // 2)
-        
+        result[-1] += (finalSum - N * (N + 1))
         return result
