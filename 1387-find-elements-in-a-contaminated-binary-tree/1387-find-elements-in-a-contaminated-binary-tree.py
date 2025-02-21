d776801e6423cc2d73f13d2dc1729e5a562e874f
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-02-21 15:35:57 +0100

    Time: 11 ms (40.11%), Space: 22.2 MB (37.43%) - LeetHub

--- 1387-find-elements-in-a-contaminated-binary-tree/1387-find-elements-in-a-contaminated-binary-tree.py
+++ 1387-find-elements-in-a-contaminated-binary-tree/1387-find-elements-in-a-contaminated-binary-tree.py
@@ -2,8 +2,7 @@ class FindElements:
     def __init__(self, root: Optional[TreeNode]):
         self.node = set()
         def decontaminate(node, val):
-            if not node:
-                return
+            if not node: return
             self.node.add(val)
             decontaminate(node.right, 2 * val + 2)
             decontaminate(node.left, 2 * val + 1)
