b93aa46617566caa782e9cf03f82ab4ac468998e
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-02-21 03:59:33 +0100

    Time: 18 ms (19.52%), Space: 22.3 MB (37.43%) - LeetHub

--- 1387-find-elements-in-a-contaminated-binary-tree/1387-find-elements-in-a-contaminated-binary-tree.py
+++ 1387-find-elements-in-a-contaminated-binary-tree/1387-find-elements-in-a-contaminated-binary-tree.py
@@ -7,23 +7,18 @@
 class FindElements:
 
     def __init__(self, root: Optional[TreeNode]):
-        self.root = root
-        self.root.val = 0
-        self.hashmap = [0]
-        cur = self.root
-        def decontaminate(node):
-            if node.left:
-                node.left.val = 2 * node.val + 1
-                self.hashmap.append(node.left.val)
-                decontaminate(node.left)
-            if node.right:
-                node.right.val = 2 * node.val + 2
-                self.hashmap.append(node.right.val)
-                decontaminate(node.right)
-        decontaminate(cur)
+        self.node = set()
+        def decontaminate(node, val):
+            if not node:
+                return
+            self.node.add(val)
+            decontaminate(node.right, 2 * val + 2)
+            decontaminate(node.left, 2 * val + 1)
+        decontaminate(root, 0)
+        print(self.node)
 
     def find(self, target: int) -> bool:
-        return target in self.hashmap
+        return target in self.node
         
 
 
