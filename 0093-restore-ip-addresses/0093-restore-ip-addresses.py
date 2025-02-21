c167e478eaf01657d495dcd7e0aba4ef51187cb1
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-02-21 17:50:02 +0100

    Time: 7 ms (20.36%), Space: 17.8 MB (46.84%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.py
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -2,7 +2,6 @@ class Solution:
     def restoreIpAddresses(self, s: str) -> List[str]:
         n = len(s)
         if n > 12 or n < 4: return []
-        valid_IP_s = r"(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])"
         valid_IP = r"^((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])$"
         res = []
         def backtrack(s, idx, i):
@@ -12,8 +11,7 @@ class Solution:
                     res.append(s[:])
                 return
             for j in range(idx + 2, m + 1):
-                if re.match(valid_IP_s, s[:j-1]):
-                    backtrack(s[:j-1] + "." + s[j-1:], j, i + 1)
+                backtrack(s[:j-1] + "." + s[j-1:], j, i + 1)
         backtrack(s, 0, 0)
         return res
 
