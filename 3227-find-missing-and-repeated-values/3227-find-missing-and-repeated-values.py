8c41524d54f8f0dfd4d57aaa4f012016c30b4370
bictorbang <92735432+bictorbang@users.noreply.github.com>
2025-03-06 13:53:07 +0100

    Time: 0 ms (100%), Space: 18.4 MB (23.37%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.py
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.py
@@ -1,9 +1,10 @@
 class Solution:
     def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
-        n_sq = len(grid)**2
+        n = len(grid)
+        n_sq = n*n
         s_a = sum(sum(x) for x in grid)
         s_e = n_sq*(n_sq + 1) / 2
-        s_a_sq = sum(sum(map(lambda x: x**2, y)) for y in grid)
+        s_a_sq = sum(sum(map(lambda x: x ** 2, y)) for y in grid)
         s_e_sq = n_sq * (n_sq + 1) * (2*n_sq + 1) / 6
         b = (s_a_sq - s_e_sq - (s_a - s_e)**2)/(2*(s_a - s_e))
         a = b + s_a - s_e
